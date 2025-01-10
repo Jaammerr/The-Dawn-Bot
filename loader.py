83fb0af6aebd8a40167a126b8b5ab3b361f8fce1
Jaammerr <andlolkek@gmail.com>
2025-01-10 12:25:00 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,14 +1,15 @@
 import asyncio
 
 from core.solvers import *
-from utils import load_config, FileOperations
+from utils import load_config, FileOperations, HeadersManager
 
 config = load_config()
 captcha_solver = (
-    AntiCaptchaImageSolver(config.anti_captcha_api_key)
+    AntiCaptchaSolver(config.anti_captcha_api_key)
     if config.captcha_module == "anticaptcha"
-    else TwoCaptchaImageSolver(config.two_captcha_api_key)
+    else TwoCaptchaSolver(config.two_captcha_api_key)
 )
+
 file_operations = FileOperations()
+headers_manager = HeadersManager()
 semaphore = asyncio.Semaphore(config.threads)
-single_semaphore = asyncio.Semaphore(1)
